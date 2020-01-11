6855cf5f07efc4eb0bfa67e2fb0b4e21fedc35e6
dhinesh-M <dhineshmail333@gmail.com>
2020-01-11 18:42:11 +0530

    table change

--- MitekMock/src/app/app.module.ts
+++ MitekMock/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- MitekMock/src/app/home/mockedResponse.ts
+++ MitekMock/src/app/home/mockedResponse.ts
@@ -1,5 +1,6 @@
 export class mockedResponse {
     public api: string;
+    public requestMethod: string;
     public statusCode: string;
     public response: string; 
 }
